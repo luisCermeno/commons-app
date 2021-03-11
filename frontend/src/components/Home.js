dfddf4172c2cbb9d59f033dfec6a6dc96db7116e
luisCermeno <luis.cermeno.f@gmail.com>
2021-03-10 21:08:54 -0800

    todo: add fetchroom view

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,29 +4,37 @@ import history from '../history'
 const Home = (props) => {
     const [roomID, setroomID] = useState('')
     const [error, seterror] = useState('')
+    const [mode, setmode] = useState('')
+
+
     const handleSubmit = e => {
       e.preventDefault()
-      //create rooom on server
-      fetch('http://localhost:8000/room/', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `JWT ${localStorage.getItem('token')}`,
-        },
-        body: JSON.stringify({
-          action: 'create',
-          roomID: roomID,
-        })
-      })
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        if (json.error === undefined){
-          history.push(`/room/${roomID}`)
-        }
-        else seterror(json.error)
-      })
-      //implement error handling here!!!
+      switch (mode){
+        case 'create':
+          //create rooom on server
+          fetch('http://localhost:8000/room/', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `JWT ${localStorage.getItem('token')}`,
+            },
+            body: JSON.stringify({
+              action: 'create',
+              roomID: roomID,
+            })
+          })
+          .then(res => res.json())
+          .then(json => {
+            console.log(json)
+            if (json.error === undefined){
+              history.push(`/room/${roomID}`)
+            }
+            else seterror(json.error)
+          })
+          break;
+        case 'join':
+          seterror('join room functionality coming soon!')
+      }
     }
 
     const handleChange = e => {
@@ -41,17 +49,52 @@ const Home = (props) => {
               console.log('hanldechange error: no name for target')
       }
     }
+
+    const handleMode = e => {
+        setroomID('')
+        setmode(e.target.name)
+    }
     return (
       <>
         <h1>{props.logged_in? `Welcome, ${props.username}`: 'Please Log In'}</h1>
-        <h3>Create a room:</h3>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor='roomID'>Room ID:</label>
-            <input name='roomID' placeholder='roomID' type='text' onChange={handleChange}/>
-            <input type='submit' value='Create' disabled={(roomID === '')}/>
-          </form>
-        <h4>{error}</h4>
+        {(mode=='')?
+        <>
+          <h3>Select an option</h3>
+          <div><button name='create' onClick= {handleMode}>Create Room</button> or <button name='join' onClick= {handleMode}>Join Room</button></div>
         </>
+        :<></>
+        }
+
+        {(mode=='create')?
+        <>
+          <h3>Create a room:</h3>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='roomID'>Room ID:</label>
+              <input name='roomID' placeholder='roomID' type='text' onChange={handleChange}/>
+              <input type='submit' value='Create' disabled={(roomID === '')}/>
+            </form>
+            <div>Do you already have a roomID? <button name='join' onClick= {handleMode}>Join room</button></div>
+          <h4>{error}</h4>
+        </>
+        :<></>
+        }
+
+        {(mode=='join')?
+        <>
+          <h3>Join a room:</h3>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='roomID'>Room ID:</label>
+              <input name='roomID' placeholder='roomID' type='text' onChange={handleChange}/>
+              <input type='submit' value='Join' disabled={(roomID === '')}/>
+            </form>
+            <div>Want to initiate a discussion? <button name='create' onClick= {handleMode}>Create room</button></div>
+          <h4>{error}</h4>
+        </>
+        :<></>
+        }
+
+      </>
+
     )
 }
 
