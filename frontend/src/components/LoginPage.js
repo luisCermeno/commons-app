95a18eb7e4e3863db55b69066996747bae53ff24
luisCermeno <luis.cermeno.f@gmail.com>
2021-03-22 18:58:29 -0700

    add logo and background

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
 import {Paper, TextField, Button} from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import jigsaw from '../img/jigsaw.png'
+import TableFooter from '@material-ui/core/TableFooter';
+import logo from '../img/logocrop.png'
 
 const LoginPage = (props) => {
     //state hooks
     const [username, setusername] = useState('')
     const [password, setpassword] = useState('')
     const [mode, setmode] = useState('login')
-
-    const windowHeight = `${window.innerHeight.toString()}px`
+    
     //effect hooks
     useEffect(() => {
         return () => {
@@ -35,15 +37,36 @@ const LoginPage = (props) => {
         <>
         <Grid
         container
-        fluid
-        direction="column"
-        justify="center" //aligns horizontally
+        direction="row"
+        justify="space-around" //aligns horizontally
         alignItems="center" //aligns vertically
-        style={{height: windowHeight}}
+        style={{height: "100vh", backgroundImage: "url(/puzzle.png)"}}
         >
-          <Grid item sm={5} xs={12}>
-            <Paper elevation={3} style={{padding: "20px 20px", textAlign: "center", maxWidth: "100%"}}>
-              <h4>The Commons</h4>
+          <Grid item md={7} sm={12}>
+            <Paper elevation={3} style={{padding: "3vh 4vw", textAlign: "center", width: "50%", borderRadius: "15px", margin: "0 auto"}}>
+              <img src={jigsaw} style={{width: "80px",display: "inline"}}/>
+              <img src={logo} style={{display: "inline"}}></img>
+              <div className="description" style={{paddingBottom: "5vh"}}>
+                <p>
+                  How many times have you wished a senior student would have told you about that bad class you regret taking so much?
+                </p>
+                <p>
+                  Are you a freshman and you feel your campus is so big that you will never find your future squad?
+                </p>
+                <p>
+                  Whether you are a senior or a freshman, a nerd or a jock. Whatever you want to know about your school, you'll find it here!
+                </p> 
+                <p>
+                  TheCommons&#174; team have envisioned a community where students learn from each other experiences
+                  and build a common knowledge about their schools! Hop on, a bring your piece to the puzzle!
+                </p>
+              </div>
+              
+            </Paper>
+          </Grid>
+          <Grid item md={5} sm={12}>
+            <Paper elevation={3} style={{padding: "4vh 4vw", textAlign: "center", width: "50%", borderRadius: "15px", margin: "0 auto"}}>
+              <h4>Discover what is going on in your school:</h4>
               {mode === 'login'?
               <div>
                 <form style={ {marginBottom: "20px",} } autoComplete="off" onSubmit={e => props.handle_login(e, {username: username, password: password})}>
@@ -83,8 +106,9 @@ const LoginPage = (props) => {
               }
             </Paper>
           </Grid>
+          
         </Grid>
-
+        
         </>
     )
     
