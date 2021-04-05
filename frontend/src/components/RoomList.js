eed3b735649d1095f3dd788d3e306eb1a6c457df
luisCermeno <luis.cermeno.f@gmail.com>
2021-04-05 16:33:10 -0700

    update responsiveness roomlist.js

--- frontend/src/components/RoomList.js
+++ frontend/src/components/RoomList.js
@@ -2,12 +2,50 @@ import {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {Grid, Paper} from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 
 const RoomList = (props) => {
+// ******** STATE HOOKS ********
 const [rooms, setrooms] = useState([])
 const [loading, setloading] = useState(true)
 
+// ******** EFFECT HOOKS *******
+useEffect(() => {
+  getRooms()
+}, [])
+
+// ******** STYLING ************
+const theme = useTheme();
+const sm = useMediaQuery(theme.breakpoints.up('sm'));
+const md = useMediaQuery(theme.breakpoints.up('md'));
+//xs and up:
+let styles = {
+  grid: {
+    border: "solid 1px black",
+    width: "100%",
+  },
+  paper: {
+    padding: "1vh 1vw", 
+    textAlign: "center", 
+    width: "100%",
+    height: "100%", 
+    borderRadius: "15px", 
+    margin: "0 auto"
+  }
+}
+//sm and up:
+if (sm) {
+  styles.grid = {...styles.grid, width: "50%"}
+  styles.paper = {...styles.paper, width: "80%"}
+}
+//lg and up:
+if (md) {
+  styles.grid = {...styles.grid, width: "25%"}
+}
+
+// ******** UTIL FUNCTIONS ********
 const getRooms = () => {
   fetch('http://localhost:8000/getroom?getall=true', {
     headers: {
@@ -22,12 +60,7 @@ const getRooms = () => {
   })
 }
 
-//get Rooms on mount
-useEffect(() => {
-  getRooms()
-}, [])
-
-
+// ******** RENDER ********
   return (
       <Grid
       container
@@ -42,8 +75,8 @@ useEffect(() => {
           <>
           {rooms.map(room =>
             (
-              <Grid item xs={12} style={{border: "solid 1px black", width: "25%"}}>
-                <Paper elevation={3} style={{padding: "1vh 1vw", textAlign: "center", width: "80%",height: "100%", borderRadius: "15px", margin: "0 auto"}}>
+              <Grid item xs={12} style={styles.grid}>
+                <Paper elevation={3} style={styles.paper}>
                   <Link 
                   to={`/room/${room.roomID}`}
                   style={{
